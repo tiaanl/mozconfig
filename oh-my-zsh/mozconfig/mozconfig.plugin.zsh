9d26b31c45cebc3968d201887e62d91ecae59a5a
Tiaan Louw <tiaanl@gmail.com>
2023-02-28 10:27:31 +0100

    Fix oh-my-zsh integration to not show errors in the prompt.

--- oh-my-zsh/mozconfig/mozconfig.plugin.zsh
+++ oh-my-zsh/mozconfig/mozconfig.plugin.zsh
@@ -2,7 +2,7 @@ MOZCONFIG_LABEL_COLOR=blue
 MOZCONFIG_CONFIG_COLOR=red
 
 function prompt_mozconfig() {
-    CONFIG=$(mozconfig)
+    CONFIG=$(mozconfig 2&>/dev/null)
     if [ ! -z $CONFIG ];
     then
         echo "%{$fg_bold[$MOZCONFIG_LABEL_COLOR]%}mozconfig:(%{$fg_bold[$MOZCONFIG_CONFIG_COLOR]%}$CONFIG%{$fg_bold[$MOZCONFIG_LABEL_COLOR]%})%{$reset_color%} "
